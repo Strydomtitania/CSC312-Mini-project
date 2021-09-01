3f8d9e02736961d0c2e750bf07022e46ed2a423d
Yusuf-Samodien <yusufsamodien5@gmail.com>
2021-09-01 03:59:51 +0200

    Created temporary login page that displays a successful or unsuccessful login.

--- AdminOperations.java
+++ AdminOperations.java
@@ -1,5 +1,7 @@
 public class AdminOperations {
     void login(){
-
+        IDandPasswords idandPasswords = new IDandPasswords();
+        
+        TempLoginPage loginPage = new TempLoginPage(idandPasswords.getLoginInfo());
     }
 }
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        AdminOperations adminOperations = new AdminOperations();
+        adminOperations.login();
+    }
+}
